6bbe126d54f58662e57fed11568fb97dd8aeac09
Paul Salaets <psalaets@gmail.com>
2015-01-28 14:21:17 -0500

    break out string handling stuff and add a few tests

---
+++ require_call.py
@@ -0,0 +1,50 @@
+import re
+
+# regex patterns
+var_name_pattern = re.compile('(\s*)(=?)(\s*)([_$a-zA-Z0-9]+)')
+lower_then_upper_pattern = re.compile('([a-z])([A-Z])')
+
+# param:    match containing 2 groups: 'a' and 'B'
+# returns: 'a-b'
+def bah(match):
+    return match.group(1) + '-' + match.group(2).lower()
+
+def camel_case_to_dashes(input):
+    if input[0].isupper():
+        input = input[0].lower() + input[1:]
+    
+    return re.sub(lower_then_upper_pattern, bah, input)
+
+def make_from(left_of_cursor):
+    # pull out var name on far side of '=', if any
+    reversed = left_of_cursor[::-1] # flip before match to make regex easier
+    
+    match = var_name_pattern.match(reversed)
+    
+    if match:
+        space_before_equals = match.group(1)
+        equals = match.group(2)
+        space_after_equals = match.group(3)
+        flipped_var_name = match.group(4)
+        
+        variable = flipped_var_name[::-1]
+        
+        # build the require() call
+        require = 'require("%s")' % camel_case_to_dashes(variable)
+        
+        result_parts = []
+        
+        if space_before_equals == '':
+            result_parts.append(' ')
+        
+        # if there wasn't an '=', add one
+        if equals == '':
+            result_parts.append('=')
+        
+        # put space after '=' if not there already
+        if space_after_equals == '':
+            result_parts.append(' ')
+        
+        result_parts.append(require)
+        
+        return ''.join(result_parts)
--- yet_another_require_plugin.py
+++ yet_another_require_plugin.py
@@ -1,19 +1,4 @@
-import sublime, sublime_plugin, re
-
-# regex patterns
-var_name_pattern = re.compile('\s*=\s*([_$a-zA-Z0-9]+)')
-lower_then_upper_pattern = re.compile('([a-z])([A-Z])')
-
-# param:    match containing 2 groups: 'a' and 'B'
-# returns: 'a-b'
-def bah(match):
-    return match.group(1) + '-' + match.group(2).lower()
-
-def camel_case_to_dashes(input):
-    if input[0].isupper():
-        input = input[0].lower() + input[1:]
-
-    return re.sub(lower_then_upper_pattern, bah, input)
+import sublime, sublime_plugin, require_call
 
 class AutoRequireModuleCommand(sublime_plugin.TextCommand):
     def run(self, edit):
@@ -26,20 +11,4 @@ class AutoRequireModuleCommand(sublime_plugin.TextCommand):
             region_up_to_cursor = sublime.Region(line.begin(), cursorPosition)
             left_side = self.view.substr(region_up_to_cursor)
             
-            # pull out var name on far side of '=', if any
-            reversed = left_side[::-1] # flip before match to make regex easier
-            match = var_name_pattern.match(reversed)
-            
-            if match:
-                # unflip the first group and that's the var name
-                variable = match.group(1)[::-1]
-            
-                # build the require() call
-                require = 'require("%s")' % camel_case_to_dashes(variable)
-            
-                # put space after '=' if not there already
-                if left_side[-1] == '=':
-                    require = ' ' + require
-                
-                # write the require to view
-                self.view.insert(edit, cursorPosition, require)
+            return require_call.make_from(left_side)
