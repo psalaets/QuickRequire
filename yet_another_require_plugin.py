7b53ca97e74d735a04630879d405781032053797
Paul Salaets <psalaets@gmail.com>
2015-01-28 15:37:12 -0500

    read quote style from settings

--- yet_another_require_plugin.py
+++ yet_another_require_plugin.py
@@ -1,5 +1,8 @@
 import sublime, sublime_plugin, require_call
 
+def quote_style:
+    return sublime.load_settings(__name__ + '.sublime-settings').get('quote_style')
+
 class AutoRequireModuleCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         for region in self.view.sel():
@@ -11,4 +14,4 @@ class AutoRequireModuleCommand(sublime_plugin.TextCommand):
             region_up_to_cursor = sublime.Region(line.begin(), cursorPosition)
             left_side = self.view.substr(region_up_to_cursor)
             
-            return require_call.make_from(left_side)
+            return require_call.make_from(left_side, quote_style())
