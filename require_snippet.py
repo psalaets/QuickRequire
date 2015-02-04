9bedf02f81ad03b052c209c7081abc5cabc17ef0
Paul Salaets <psalaets@gmail.com>
2015-02-04 14:44:24 -0500

    rename

--- require_snippet.py
+++ require_snippet.py
@@ -7,14 +7,14 @@ lower_then_upper_pattern = re.compile('([a-z])([A-Z])')
 
 # param:    match containing 2 groups: 'a' and 'B'
 # returns: 'a-b'
-def bah(match):
+def to_dashed(match):
     return match.group(1) + '-' + match.group(2).lower()
 
 def module_name(input):
     if input[0].isupper():
         input = input[0].lower() + input[1:]
     
-    return re.sub(lower_then_upper_pattern, bah, input)
+    return re.sub(lower_then_upper_pattern, to_dashed, input)
 
 def make_from(left_of_cursor, quote_style='double'):
     if quote_style == 'single':
